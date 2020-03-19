0535ce8d07c309a409947850bcf4635285bf8807
jeonghoheo <heojeongho1992@gmail.com>
2020-03-19 20:24:54 +0900

    add - Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ const plus = document.getElementById("plus"),
   minus = document.getElementById("minus"),
   number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState());
