3355c0aac46cfdbf42e95f521ce731eb6ceb227f
jeonghoheo <heojeongho1992@gmail.com>
2020-05-04 10:34:07 +0900

    mod - replace reducer with CreateReducer

--- src/store.js
+++ src/store.js
@@ -1,22 +1,32 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
+const initialState = [];
 const ADD = "ADD",
   DELETE = "DELETE";
 
 const addToDo = createAction(ADD);
 const deleteToDo = createAction(DELETE);
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.toString():
-      return [{ text: action.payload, id: Date.now(), ...state }];
-    case deleteToDo.toString():
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.toString():
+//       return [{ text: action.payload, id: Date.now(), ...state }];
+//     case deleteToDo.toString():
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer(initialState, {
+  addToDo: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  deleteToDo: (state, action) => {
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
