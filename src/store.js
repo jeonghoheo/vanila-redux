108fb3b59f085be8d0470d930be947eee68965e4
jeonghoheo <heojeongho1992@gmail.com>
2020-05-04 10:36:37 +0900

    fix - createReducer 안의 객체를 인덱싱으로 접근하게 하기

--- src/store.js
+++ src/store.js
@@ -8,22 +8,11 @@ const ADD = "ADD",
 const addToDo = createAction(ADD);
 const deleteToDo = createAction(DELETE);
 
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.toString():
-//       return [{ text: action.payload, id: Date.now(), ...state }];
-//     case deleteToDo.toString():
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
-
 const reducer = createReducer(initialState, {
-  addToDo: (state, action) => {
+  [addToDo]: (state, action) => {
     state.push({ text: action.payload, id: Date.now() });
   },
-  deleteToDo: (state, action) => {
+  [deleteToDo]: (state, action) => {
     return state.filter((toDo) => toDo.id !== action.payload);
   },
 });
